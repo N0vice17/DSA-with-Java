742cad6ed71c621f3f32fea1814d1a2a04fc0442
Debojit Ganguly <debojitganguly69@gmail.com>
2022-12-14 19:47:30 +0530

    Added code for bubble sort written by me

--- Bubble_sort.java
+++ Bubble_sort.java
@@ -1,26 +1,17 @@
 import java.util.Arrays;
 
 public class Bubble_sort {
-  static void bubble_sort(int[]arr){
-    boolean swapped;
-    for(int i=0;i<arr.length;i++){
-      swapped=false;
-      for(int j=1;j<arr.length-i;j++){
-        if(arr[j]<arr[j-1]){
-        int temp=arr[j-1];
-        arr[j-1]=arr[j];
-        arr[j]=temp;
-        swapped=true;
-        }
-        if(swapped==false){
-          break;
-        }
-      }
-    }
-  }
   public static void main(String[]args){
     int[] arr={9,8,7,6,5,4,3,2,1};
-    bubble_sort(arr);
+    for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr.length-i-1;j++) {
+				if(arr[j]>arr[j+1]) {
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
     System.out.println(Arrays.toString(arr));
   }
 }
--- Practice1.java
+++ Practice1.java
@@ -1,4 +1,3 @@
-import java.math.BigInteger;
 import java.util.*;
 public class Practice1 {
     public static void main(String[] args) {
