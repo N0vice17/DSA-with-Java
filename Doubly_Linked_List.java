53ff3215020b9accdaa1347d818f1836ee5d5568
Debojit Ganguly <debojitganguly69@gmail.com>
2022-12-29 12:26:52 +0530

    Adding some files and deleting some files

--- Doubly_Linked_List.java
+++
@@ -1,5 +0,0 @@
-public class Doubly_Linked_List {
-    public static void main(String[]args){
-        
-    }
-}
--- Insertion_sort.java
+++ Insertion_sort.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class Insertion_sort {
-    static void Sort(int[]arr){
+    public static void Sort(int[]arr){
         for(int i=0;i<arr.length-1;i++){
             for(int j=i+1;j>0;j--){
                 if(arr[j]<arr[j-1]){
@@ -13,7 +13,7 @@ public class Insertion_sort {
             }
         }
     }
-    static void swap(int[]arr,int index1,int index2){
+    public static void swap(int[]arr,int index1,int index2){
         int temp=arr[index2];
         arr[index2]=arr[index1];
         arr[index1]=temp;
